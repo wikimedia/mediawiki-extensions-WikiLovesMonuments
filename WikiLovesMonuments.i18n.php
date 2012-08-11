709c760fd317eb92b94fb991bf07554cf84067cd
Platonides <platonides@gmail.com>
2012-08-11 20:25:37 +0200

    Add message documentation.

--- WikiLovesMonuments.i18n.php
+++ WikiLovesMonuments.i18n.php
@@ -51,3 +51,21 @@ $messages['es'] = array(
 	'wlm-sponsors' => 'Patrocinadores',
 	'wlm-uploading' => 'Subir fotos',
 );
+
+$messages['qqq'] = array(
+	'wlm-extension-desc' => 'Description of the WikiLovesMonumentsw extension.',
+	'wlm-no-contest-year' => 'Error message given when WLM parser functions get passed a parameter which is not a year in which there was (or is planned) a WLM edition.',
+
+	'wlm-contact' => 'Caption for a page about contacting the (local) organization.',
+	'wlm-contest' => 'Contest, competition. What Wiki Loves Monuments is. Suitable as caption for a page talking about Wiki Loves Monuments.',
+	'wlm-donate' => 'Caption for a page about donating to the chapter.',
+	'wlm-faq' => 'Text suitable as caption for a link leading to a list of Questions and answers (Frequently Asked Questions) about the Wiki Loves Monuments contest',
+	'wlm-jury' => 'Jury, people who will decide which photos are the best. Suitable as caption for a page talking about the local jury, presenting it, explaining how the best photos are selected or what kind of people will decide.',
+	'wlm-monuments' => 'Monuments, what you photograph in Wiki Loves Monuments. Suitable as caption for a page talking about which monuments are eligible, or with the monument lists.',
+	'wlm-news' => 'News, current events. Suitable as a caption for a page/blog giving the latest information about the contest.',
+	'wlm-participate' => 'Text moving the user to participate in the Wiki Loves Monuments contest, preferably some kind of imperative. Suitable as caption for a link to a page explaining how to participate in a few simple steps.',
+	'wlm-prizes' => 'Prizes, awards, what you will give to the authors of the best photos. Suitable for leading to a page describing them.',
+	'wlm-rules' => 'Rules, the thing that people must abide by for participating in Wiki Loves Monuments. Caption suitable for a page with a boring list of things that most people won\'t actually need.',
+	'wlm-sponsors' => 'Caption suitable for leading to a page about the sponsors/partners of the Wiki Loves Monuments contest.',
+	'wlm-uploading' => 'Text suitable for leading to a page with instructions / explaining how to upload the photos.',
+);
