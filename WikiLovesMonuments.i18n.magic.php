884173c33956351666a674d46153c434b95aa207
Platonides <platonides@gmail.com>
2012-08-15 00:28:00 +0200

    Add the magic for {{#wlm-country-website: }}
    Change-Id: I5d52d1f34a3dff6dd1f4849cfef0b343237d3bd4

--- WikiLovesMonuments.i18n.magic.php
+++ WikiLovesMonuments.i18n.magic.php
@@ -11,5 +11,6 @@ $magicWords = array();
 $magicWords['en'] = array(
    'wlm-countries' => array( 0, 'wlm-countries' ),
    'wlm-country-count' => array( 0, 'wlm-country-count' ),
+   'wlm-country-website' => array( 0, 'wlm-country-website' ),
 );
 
